f1c556f376eccce8d3c50aaac6328b34d254f337
RestingaPUC <carneiroalberto@yahoo.com.br>
2022-09-25 17:04:09 -0300

    first commit

---
+++ src/main/java/course/secao25/Secao25Application.java
@@ -0,0 +1,13 @@
+package course.secao25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Secao25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Secao25Application.class, args);
+	}
+
+}
---
+++ src/test/java/course/secao25/Secao25ApplicationTests.java
@@ -0,0 +1,13 @@
+package course.secao25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Secao25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
